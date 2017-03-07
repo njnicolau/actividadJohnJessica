dea3e0ee7bd75147cdf8534eb8c63363d0a9a0fa
guest-yhlpwq <guest-yhlpwq@Ares>
2017-03-07 20:39:55 +0100

    Resolt bug

--- index.php
+++ index.php
@@ -4,5 +4,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor. Versio1
+ * Aquesta es una linea de Jesica
+ * I aquesta una altra
  */
 
